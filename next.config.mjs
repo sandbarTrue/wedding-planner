1e0ca3534619b01a12a87b80d32a59af308d2f38
瓦力 <wali@junaitools.com>
2026-02-25 17:31:32 +0800

    sync: latest code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/wedding',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
